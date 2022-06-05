e0da8d0751094828e5226a626e417a6c5dcfe214
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-05 11:36:07 +0200

    ✨ [save] Add .jpeg support

--- src/Save.cpp
+++ src/Save.cpp
@@ -19,4 +19,20 @@ void save_png(const char*    file_path,
     stbi_write_png(file_path, static_cast<int>(width), static_cast<int>(height), channels_count, data, 0);
 }
 
+void save_jpeg(std::filesystem::path file_path, const Image& image, bool flip_vertically)
+{
+    save_jpeg(file_path.string().c_str(), image.width(), image.height(), image.data(), image.channels_count(), flip_vertically);
+}
+
+void save_jpeg(const char*    file_path,
+               Size::DataType width,
+               Size::DataType height,
+               const void*    data,
+               int            channels_count,
+               bool           flip_vertically)
+{
+    stbi_flip_vertically_on_write(flip_vertically ? 1 : 0);
+    stbi_write_jpg(file_path, static_cast<int>(width), static_cast<int>(height), channels_count, data, 100);
+}
+
 } // namespace img

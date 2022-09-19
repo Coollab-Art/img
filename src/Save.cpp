c66b76e94edf1386ef583630e252f856eb5adef4
Jules Fouchy <jules.fouchy@ntymail.com>
2022-09-19 13:10:56 +0200

    ♻️ [Save] Use std::filesystem::path instead of const char*

--- src/Save.cpp
+++ src/Save.cpp
@@ -5,18 +5,18 @@ namespace img {
 
 void save_png(std::filesystem::path file_path, const Image& image, bool flip_vertically)
 {
-    save_png(file_path.string().c_str(), image.width(), image.height(), image.data(), image.channels_count(), flip_vertically);
+    save_png(file_path, image.width(), image.height(), image.data(), image.channels_count(), flip_vertically);
 }
 
-void save_png(const char*    file_path,
-              Size::DataType width,
-              Size::DataType height,
-              const void*    data,
-              int            channels_count,
-              bool           flip_vertically)
+void save_png(std::filesystem::path file_path,
+              Size::DataType        width,
+              Size::DataType        height,
+              const void*           data,
+              int                   channels_count,
+              bool                  flip_vertically)
 {
     stbi_flip_vertically_on_write(flip_vertically ? 1 : 0);
-    stbi_write_png(file_path, static_cast<int>(width), static_cast<int>(height), channels_count, data, 0);
+    stbi_write_png(file_path.string().c_str(), static_cast<int>(width), static_cast<int>(height), channels_count, data, 0);
 }
 
 void save_jpeg(std::filesystem::path file_path, const Image& image, bool flip_vertically)

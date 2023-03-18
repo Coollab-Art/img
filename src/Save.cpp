5f3381799ef6b6591b28e6c77eb625f1ccdbe742
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-18 23:52:41 +0100

    🧼 Cleanup

--- src/Save.cpp
+++ src/Save.cpp
@@ -3,40 +3,40 @@
 
 namespace img {
 
-void save_png(std::filesystem::path file_path, const Image& image, bool flip_vertically)
+void save_png(std::filesystem::path file_path const&, Image const& image, bool flip_vertically)
 {
     save_png(file_path, image.width(), image.height(), image.data(), image.channels_count(), flip_vertically);
 }
 
 void save_png(
-    std::filesystem::path file_path,
-    Size::DataType        width,
-    Size::DataType        height,
-    const void*           data,
-    int                   channels_count,
-    bool                  flip_vertically
+    std::filesystem::path const& file_path,
+    Size::DataType               width,
+    Size::DataType               height,
+    const void*                  data,
+    int                          channels_count,
+    bool                         flip_vertically
 )
 {
     stbi_flip_vertically_on_write(flip_vertically ? 1 : 0);
     stbi_write_png(file_path.string().c_str(), static_cast<int>(width), static_cast<int>(height), channels_count, data, 0);
 }
 
-void save_jpeg(std::filesystem::path file_path, const Image& image, bool flip_vertically)
+void save_jpeg(std::filesystem::path const& file_path, Image const& image, bool flip_vertically)
 {
     save_jpeg(file_path.string().c_str(), image.width(), image.height(), image.data(), image.channels_count(), flip_vertically);
 }
 
 void save_jpeg(
-    const char*    file_path,
-    Size::DataType width,
-    Size::DataType height,
-    const void*    data,
-    int            channels_count,
-    bool           flip_vertically
+    std::filesystem::path const& file_path,
+    Size::DataType               width,
+    Size::DataType               height,
+    void const*                  data,
+    int                          channels_count,
+    bool                         flip_vertically
 )
 {
     stbi_flip_vertically_on_write(flip_vertically ? 1 : 0);
-    stbi_write_jpg(file_path, static_cast<int>(width), static_cast<int>(height), channels_count, data, 100);
+    stbi_write_jpg(file_path.string().c_str(), static_cast<int>(width), static_cast<int>(height), channels_count, data, 100);
 }
 
 } // namespace img

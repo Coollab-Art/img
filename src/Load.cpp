177e5a543e762734d3367b977ba498c59679bcf3
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-18 23:50:30 +0100

    ⬆️ [tooling]

--- src/Load.cpp
+++ src/Load.cpp
@@ -10,11 +10,14 @@ Image load(std::filesystem::path file_path, int desired_channels_count, bool fli
     stbi_set_flip_vertically_on_load(flip_vertically ? 1 : 0);
     int      w, h; // NOLINT
     uint8_t* data = stbi_load(file_path.string().c_str(), &w, &h, nullptr, desired_channels_count);
-    if (!data) {
+    if (!data)
+    {
         throw std::runtime_error{"[img::load] Couldn't load image from \"" + file_path.string() + "\":\n" + stbi_failure_reason()};
     }
-    return Image{{static_cast<Size::DataType>(w),
-                  static_cast<Size::DataType>(h)},
+    return Image{{
+                     static_cast<Size::DataType>(w),
+                     static_cast<Size::DataType>(h),
+                 },
                  desired_channels_count,
                  data};
 }
--- src/Save.cpp
+++ src/Save.cpp
@@ -8,12 +8,14 @@ void save_png(std::filesystem::path file_path, const Image& image, bool flip_ver
     save_png(file_path, image.width(), image.height(), image.data(), image.channels_count(), flip_vertically);
 }
 
-void save_png(std::filesystem::path file_path,
-              Size::DataType        width,
-              Size::DataType        height,
-              const void*           data,
-              int                   channels_count,
-              bool                  flip_vertically)
+void save_png(
+    std::filesystem::path file_path,
+    Size::DataType        width,
+    Size::DataType        height,
+    const void*           data,
+    int                   channels_count,
+    bool                  flip_vertically
+)
 {
     stbi_flip_vertically_on_write(flip_vertically ? 1 : 0);
     stbi_write_png(file_path.string().c_str(), static_cast<int>(width), static_cast<int>(height), channels_count, data, 0);
@@ -24,12 +26,14 @@ void save_jpeg(std::filesystem::path file_path, const Image& image, bool flip_ve
     save_jpeg(file_path.string().c_str(), image.width(), image.height(), image.data(), image.channels_count(), flip_vertically);
 }
 
-void save_jpeg(const char*    file_path,
-               Size::DataType width,
-               Size::DataType height,
-               const void*    data,
-               int            channels_count,
-               bool           flip_vertically)
+void save_jpeg(
+    const char*    file_path,
+    Size::DataType width,
+    Size::DataType height,
+    const void*    data,
+    int            channels_count,
+    bool           flip_vertically
+)
 {
     stbi_flip_vertically_on_write(flip_vertically ? 1 : 0);
     stbi_write_jpg(file_path, static_cast<int>(width), static_cast<int>(height), channels_count, data, 100);

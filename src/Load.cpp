6da914f8a12108b775272d6a25545ea4b93812cc
Jules Fouchy <jules.fouchy@ntymail.com>
2024-08-07 12:07:46 +0200

    💥 Image now stores info about RGB vs BGR, and whether the first row is the bottom or top of the image

--- src/Load.cpp
+++ src/Load.cpp
@@ -2,15 +2,16 @@
 #include <stb_image/stb_image.h>
 #include <stdexcept>
 #include <string>
+#include "Image.h"
 
 namespace img {
 
-Image load(std::filesystem::path file_path, std::optional<int> desired_channels_count, bool flip_vertically)
+Image load(std::filesystem::path const& file_path, std::optional<int> desired_channels_count, img::FirstRowIs desired_row_order)
 {
     assert((!desired_channels_count.has_value() || *desired_channels_count != 0) && "If you don't want to enforce a channels count, don't set desired_channels_count to 0, but to std::nullopt");
     assert(!desired_channels_count.has_value() || *desired_channels_count == 3 || *desired_channels_count == 4);
 
-    stbi_set_flip_vertically_on_load(flip_vertically ? 1 : 0);
+    stbi_set_flip_vertically_on_load(desired_row_order == img::FirstRowIs::Bottom ? 1 : 0);
     int      w, h, actual_channels_count_in_file; // NOLINT
     uint8_t* data = stbi_load(file_path.string().c_str(), &w, &h, &actual_channels_count_in_file, desired_channels_count.value_or(0));
     if (!data)
@@ -21,7 +22,8 @@ Image load(std::filesystem::path file_path, std::optional<int> desired_channels_
             static_cast<Size::DataType>(w),
             static_cast<Size::DataType>(h),
         },
-        desired_channels_count.value_or(actual_channels_count_in_file),
+        desired_channels_count.value_or(actual_channels_count_in_file) == 3 ? img::PixelFormat::RGB : img::PixelFormat::RGBA,
+        desired_row_order,
         data,
     };
 }
